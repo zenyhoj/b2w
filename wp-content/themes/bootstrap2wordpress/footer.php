5e523564570effc5f799180e4cae54ee7db6312d
Joe Balingit <joe.balingit@gmail.com>
2017-03-16 14:00:11 +0800

    coded until page-home.php

--- wp-content/themes/bootstrap2wordpress/footer.php
+++ wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,18 +11,89 @@
 
 ?>
 
-	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="https://automattic.com/" rel="designer">Joe Balingit</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <p><a href="#"><img src="<?php bloginfo(stylesheet_directory); ?>/assets/img/b2w.png" alt="b2w logo"></a></p>
+                </div>
 
-<?php wp_footer(); ?>
+                <div class="col-sm-6">
+                    <nav>
+                        <ul class="list-unstyled list-inline">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#blog">Blog</a></li>
+                            <li><a href="#resources">Resources</a></li>
+                            <li><a href="">Contact</a></li>
+                            <li  class="signup-link"><a href="/">Signup now</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="col-sm-3">
+                    <p class="pull-right">
+                        &copy;2014 Brad Hussey
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+    <!-- Modal 
+        =============================================-->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <!--<span aria-hidden="true">&times;</span>-->
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                        <span class="sr-only">Close</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our Mailing List</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Simply enter your name and email! As a thank you for joining us, we're going to give you one of our best-selling
+                        courses, <em>for free!</em></p>
+                </div>
+                <form class="form-inline text-center" role="form">
+                    <div class="form-group">
+                        <label class="sr-only" for="subscribe-name">Your Firstname</label>
+                        <input type="text" class="form-control" id="subscribe-name" placeholder="Your firstname">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="sr-only" for="subscribe-email">Your Email</label>
+                        <input type="text" class="form-control" id="subscribe-email" placeholder="Your email">
+                    </div>
+                    <input type="submit" class="btn btn-danger" value="Subscribe!">
+                </form>
+                <br>
+                <div class="modal-footer text-muted">
+                    <p class="small text-center">By providing your email, you consent to receiving occassional promotional emails &amp; newsletters. <br>No
+                        Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</p>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+ 
+ <?php wp_footer(); ?>
+ <!-- Bootstrap core JS 
+        =============================================-->
+    <!--<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-3.1.1.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+	
+
+
+
+
 
 </body>
 </html>
--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -18,13 +18,13 @@
 
 <!--Bootstrap core css-->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/bootstrap.min.css">
-
-
-    <!--favicon-->
+<!--favicon-->
 <link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/assets/img/favicon.ico">
     <!--font-awesome icons-->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" media="all" href='<?php bloginfo( 'style.css' ); ?>'>
     <!--Google fonts -->
+
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css">
     <!--<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">-->
 
@@ -32,8 +32,11 @@
 
 
 <?php wp_head(); ?>
-<!--[if lt IE 7]>
-           <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
 </head>
