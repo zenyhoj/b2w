6b2f0be33d4521ce19c6203d2e6e00977deb061a
Joe Balingit <joe.balingit@gmail.com>
2017-03-21 09:50:12 +0800

    used acf plugin and coded boost your income section

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -13,6 +13,16 @@ $button_text            =   get_post_meta(7, 'button_text', true);
 $opt_in                 =   get_post_meta(7, 'opt_in', true);
 $optin_button_text      =   get_post_meta(7, 'optin_button_text', true);
 
+//Using Advanced Custom Fields Plugin
+//Boost Your Income Section
+$income_feature_image   =   get_field('income_feature_image');
+$income_section_title   =   get_field('income_section_title');
+$income_section_desc    =   get_field('income_section_description');
+$reason_1_title         =   get_field('reason_1_title');
+$reason_1_desc          =   get_field('reason_1_description');
+$reason_2_title         =   get_field('reason_2_title');
+$reason_2_desc          =   get_field('reason_2_description');
+
 
 get_header(); ?>
 
@@ -74,43 +84,31 @@ get_header(); ?>
 
     </section>
 
-    <!-- Boost your income 
+    <!-- Boost your income section
         =============================================-->
     <section id="boost-income">
         <div class="container">
             <div class="section-header">
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-boost.png" alt="Chart">
-                <h2>How You Can Boost Your Income</h2>
-            </div>
-            <!--section-header-->
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, veniam quis consectetur. Est amet, dolores omnis
-                maxime commodi dignissimos voluptas saepe tempore magnam facilis fugit, incidunt explicabo cum voluptates.
-                Autem.
-            </p>
 
+            <!--if user uploaded an image-->
+            
+            <?php  if( !empty($income_feature_image) ) :   ?>
+            
+                <img src="<?php echo $income_feature_image['url']; ?>" alt="<?php $income_feature_image['alt']; ?>">
+            
+            <?php endif; ?>
+            <h2><?php echo $income_section_title;?></h2>
+            </div> <!--section-header-->
+            <p class="lead" ><?php echo $income_section_desc; ?></p>
             <div class="row">
                 <div class="col-sm-6">
-                    <h3>Make money on the side</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At neque odio enim officiis, vitae praesentium
-                        tenetur, mollitia labore perferendis, magni dicta et. Facilis consequatur officiis veritatis, nemo
-                        sed cupiditate voluptate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque veniam
-                        repudiandae ab. Iure fugit, minima adipisci, illum maxime rerum labore, magnam dolores harum necessitatibus,
-                        eos soluta reiciendis nulla itaque incidunt! Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Quas odio quaerat veritatis veniam nulla, ex ipsa vitae quos consectetur, dolor enim magnam
-                        temporibus error voluptatum incidunt provident quod! Molestias, voluptatibus.
-                    </p>
+                    <h3><?php echo $reason_1_title; ?></h3>
+                    <p><?php echo $reason_1_desc; ?></p>
                 </div>
                 <!--col-->
                 <div class="col-sm-6">
-                    <h3>Create a full time income</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At neque odio enim officiis, vitae praesentium
-                        tenetur, mollitia labore perferendis, magni dicta et. Facilis consequatur officiis veritatis, nemo
-                        sed cupiditate voluptate! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque veniam
-                        repudiandae ab. Iure fugit, minima adipisci, illum maxime rerum labore, magnam dolores harum necessitatibus,
-                        eos soluta reiciendis nulla itaque incidunt! Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Quas odio quaerat veritatis veniam nulla, ex ipsa vitae quos consectetur, dolor enim magnam
-                        temporibus error voluptatum incidunt provident quod! Molestias, voluptatibus.
-                    </p>
+                    <h3><?php echo $reason_2_title; ?></h3>
+                    <p><?php echo $reason_2_desc; ?></p>
                 </div>
                 <!--col-->
             </div>
