73b393ff07c28b4865a0311bc98c4ba2757c9f90
Joe Balingit <joe.balingit@gmail.com>
2017-03-24 10:34:40 +0800

    coded the testimonial section using cpt

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -318,60 +318,26 @@ get_header(); ?>
             <div class="row">
                 <div class="col-sm-8 col-sm-offset-2">
                     <h2>What People Are Saying About Brad</h2>
-                    
+
+                     <?php $loop = new WP_Query(array('post_type' => 'testimonial', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+                        <?php while($loop->have_posts()) : $loop->the_post(); ?>
+                        <div class="row testimonial">
+                        <div class="col-sm-4">
+                            
+                            <?php if(has_post_thumbnail( )) {
+                                the_post_thumbnail(array(200, 200));
+                            }
+                            ?>
+                        </div><!--endcol-->
                     <!--Testimonial-->
-                    <div class="row testimonial">
-                        <div class="col-sm-4">
-                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brennan.jpg" alt="Brennan">
-                        </div>
-
                         <div class="col-sm-8">
                             <blockquote>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim illum qui commodi eveniet, similique deleniti inventore voluptates exercitationem temporibus nulla, reprehenderit quam, ut sit sequi vel dolores in eos, explicabo!
-                            <cite>&mdash; Brenna, Codecollege Grad</cite>
+                                <?php the_content()?>
+                            <cite>&mdash; <?php the_title(); ?></cite>
                             </blockquote>
                         </div>
                     </div><!--end of testimonial-->
-
-                       <!--Testimonial-->
-                    <div class="row testimonial">
-                        <div class="col-sm-4">
-                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Ben with moustache">
-                        </div>
-
-                        <div class="col-sm-8">
-                            <blockquote>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim illum qui commodi eveniet, similique deleniti inventore voluptates exercitationem temporibus nulla, reprehenderit quam, ut sit sequi vel dolores in eos, explicabo!
-                            <cite>&mdash; Brenna, Codecollege Grad</cite>
-                            </blockquote>
-                        </div>
-                    </div><!--end of testimonial-->
-
-                       <!--Testimonial-->
-                    <div class="row testimonial">
-                        <div class="col-sm-4">
-                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="Aj">
-                        </div>
-                        <div class="col-sm-8">
-                            <blockquote>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim illum qui commodi eveniet, similique deleniti inventore voluptates exercitationem temporibus nulla, reprehenderit quam, ut sit sequi vel dolores in eos, explicabo!
-                            <cite>&mdash; Brenna, Codecollege Grad</cite>
-                            </blockquote>
-                        </div>
-                    </div><!--end of testimonial-->
-
-                       <!--Testimonial-->
-                    <div class="row testimonial">
-                        <div class="col-sm-4">
-                            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Brennan">
-                        </div>
-                        <div class="col-sm-8">
-                            <blockquote>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim illum qui commodi eveniet, similique deleniti inventore voluptates exercitationem temporibus nulla, reprehenderit quam, ut sit sequi vel dolores in eos, explicabo!
-                            <cite>&mdash; Brenna, Codecollege Grad</cite>
-                            </blockquote>
-                        </div>
-                    </div><!--end of testimonial-->
+                    <?php endwhile; ?>
                 </div>
             </div>
         </div>
